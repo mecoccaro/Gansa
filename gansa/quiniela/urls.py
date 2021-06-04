a2255f96cd790aeabfd24b99de2a7d7fa2ac49a0
mecoccaro <miguele.coccaro@yahoo.com>
2021-06-04 19:10:16 -0400

    Signup con detalles

--- gansa/quiniela/urls.py
+++ gansa/quiniela/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = \
     [
         path('', views.index, name='index'),
-        url(r'^signup/$', views.signup, name='signup'),
+        #url(r'^signup/$', views.signup, name='signup'),
+        url(r'^signup/$', views.register, name='signup'),
     ]
--- gansa/quiniela/views.py
+++ gansa/quiniela/views.py
@@ -1,24 +1,30 @@
-from django.shortcuts import render
-from django.template import loader
-from .models import *
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
 
 def index(request):
     return HttpResponse("Hola, mundo. Está en el índice de encuestas.")
 
-def signup(request):
+def register(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        context = {'form': form}
+        return render(request, 'registration/signup.html', context)
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+        form = RegisterForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+        user = form.cleaned_data.get('username')
+        messages.success(request, 'Account was created for ' + user)
+        return redirect('/accounts/login')
     else:
-        form = UserCreationForm()
-    return render(request, '../templates/registration/signup.html', {'form': form})
+        print('Form is not valid')
+        messages.error(request, 'Error Processing Your Request')
+        context = {'form': form}
+        return render(request, 'registration/signup.html', context)
+
+    return render(request, 'registration/signup.html', {})
+
