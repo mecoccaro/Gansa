e644bf4939011cd378f54e3b6bd7fd0ea1f80e2d
mecoccaro <miguele.coccaro@yahoo.com>
2021-06-03 20:15:49 -0400

    login template

--- gansa/gansa/settings.py
+++ gansa/gansa/settings.py
@@ -131,7 +131,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
