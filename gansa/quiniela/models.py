fcf7158a281578d3f313caf95f54640a1e943031
mecoccaro <miguele.coccaro@yahoo.com>
2021-06-04 16:13:41 -0400

    Arreglos en login

--- gansa/quiniela/models.py
+++ gansa/quiniela/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User as DJuser
+from django.contrib.auth.forms import UserCreationForm
 
 class Tournament(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- gansa/quiniela/views.py
+++ gansa/quiniela/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from django.template import loader
+from .models import *
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def index(request):
     return HttpResponse("Hola, mundo. Está en el índice de encuestas.")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, '../templates/registration/signup.html', {'form': form})
