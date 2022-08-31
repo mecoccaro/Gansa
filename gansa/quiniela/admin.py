813a499ac0a6e974ca957883cc62aa2695319dab
Miguel Coccaro <miguele.coccaro@yahoo.com>
2022-08-31 13:55:49 -0400

    refactor database and admin panel update

--- gansa/quiniela/admin.py
+++ gansa/quiniela/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Teams, Game, QuinielaTournament
 
-# Register your models here.
+
+class TeamsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'type')
+    search_fields = ['name']
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'init_date', 'end_date')
+    search_fields = ['name']
+    
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'game', 'phase', 'winner')
+    search_fields = ['id']
+    model = Teams
+
+    def game(self, obj):
+        res = str(obj.teamA) + '-' + str(obj.teamB)
+        return res
+
+
+admin.site.register(Teams, TeamsAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(QuinielaTournament, TournamentAdmin)
