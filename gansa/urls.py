b5f32936a85f9312e3e2a1047b6ea007ebd82a43
mecoccaro <miguele.coccaro@yahoo.com>
2024-06-12 00:41:45 -0400

    terms and conditions

--- gansa/urls.py
+++ gansa/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('quiniela/', include('quiniela.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('terms/', TemplateView.as_view(template_name='terms.html'), name='home'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
