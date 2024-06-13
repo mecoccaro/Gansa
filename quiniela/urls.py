6a708c25cf755ef74e0cb8b1e6a8bb67a6b6fceb
mecoccaro <miguele.coccaro@yahoo.com>
2024-06-13 00:46:51 -0400

    easter egg and more

--- quiniela/urls.py
+++ quiniela/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 from django.views.defaults import page_not_found
 
